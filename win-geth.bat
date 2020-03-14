1c625ebe1080a2206daf8060181fa7b4ba18f9d5
高野 <gaoye@tokenpai.com>
2020-03-14 21:03:26 +0800

    windows-bat-file

---
+++ win-geth.bat
@@ -0,0 +1,1 @@
+geth --datadir ./data --networkid 18 --port 30303 --rpc --rpcaddr 0.0.0.0 --rpcvhosts "*"  --rpcport 8545 --rpcapi 'db,net,eth,web3,personal' --rpccorsdomain '*' --allow-insecure-unlock  console 2> 1.log
