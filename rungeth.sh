a0e41871a9f16e4da909c5f87515965343c97686
yekai1003 <yekai_23@sohu.com>
2020-04-18 15:18:38 +0800

    sh

--- rungeth.sh
+++ rungeth.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-geth --datadir ./data --networkid 18 --port 30303 --rpc  --rpcport 8545 --rpcapi 'db,net,eth,web3,personal' --rpccorsdomain '*' --gasprice 0 --allow-insecure-unlock  console 2> 1.log
+geth --datadir ./data --networkid 18 --port 30303 --rpc  --rpcport 8545 --rpcapi 'db,net,eth,web3,personal' --rpccorsdomain "*" --gasprice 0 --allow-insecure-unlock  console 2> 1.log
